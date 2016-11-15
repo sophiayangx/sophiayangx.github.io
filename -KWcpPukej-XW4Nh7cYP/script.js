2f24b58d77ada84b6d5e8073f8d1bfe45d63b386
sophiayangx <sopyang@nuevaschool.org>
2016-11-15 08:45:36 -0800

    Classadoo autocommit

---
+++ -KWcpPukej-XW4Nh7cYP/script.js
@@ -0,0 +1,46 @@
+var shown = true
+setInterval(function() {
+  if (shown) {
+    $(".blinking").css({"visibility": "hidden"});
+    shown = false;
+  } else {		
+    $(".blinking").css({"visibility": "visible"});
+    shown = true;
+  }
+}, 500)
+
+
+var lettersToFade = $(".by-letter-fade").children();
+var index = 0
+setInterval(function() {
+  if (index >= lettersToFade.length) {
+    index = 0
+  }
+
+  var letterToFade = $(lettersToFade[index]);
+
+  letterToFade.animate({"opacity": "0"}, {
+    complete: function() {
+      letterToFade.animate({opacity: "1"})
+    }
+  }) 	
+
+  index = index + 1
+}, 500)
+
+
+$(document).ready(function() {
+  $('#custom').mouseenter(function() {
+    $(this).animate({
+      height: '+=10px'
+    });
+  });
+  $('#custom').mouseleave(function() {
+    $(this).animate({
+      height: '-=10px'
+    }); 
+  });
+  $('#colorblock').click(function() {
+    $(this).toggle(1000);
+  }); 
+});
